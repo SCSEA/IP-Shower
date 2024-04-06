131ee5037ae5af79deaa983f1edd0da71c1885bd
SCSEA <138138604+SCSEA@users.noreply.github.com>
2024-04-06 14:11:17 +0300

    Update ip-shower.py

--- ip-shower.py
+++ ip-shower.py
@@ -6,11 +6,10 @@ print("[+] Version:1.0")
 
 print("[+] Team:Junior Programmers")
 
-print("[+] Github:https://github.com/Yousuf9963/phone-num-info")
+print("[+] Github:https://github.com/SCSEA")
 
-print("[+] Follow me on Github: https://github.com/Yousuf9963")
+print("[+] Follow me on Github: https://github.com/SCSEA")
 
-print("[-] Website: muhammadabdirahman.wixsite.com/yousuf9963blog.")
 
 print("[!] legal disclaimer: Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
 
